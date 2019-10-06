a31bb377eb79d0e3d9980105652d6c1b63cf1bf8
robhyrk <rob.hyrkiel@gmail.com>
2019-10-06 00:17:47 -0400

    feat: created custom button component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import SignPage from './pages/sign-page/sign-page.component'
+
 import Header from './components/header/header.component'
+
 
 function App() {
   return (
@@ -13,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage}/>
         <Route path="/shop" component={ShopPage}/>
+        <Route path="/sign-in" component={SignPage}/>
       </Switch>
     </div>
   );
