81c6531bf6964deb0d4b94b3c51a44993ae176ec
robhyrk <rob.hyrkiel@gmail.com>
2019-10-09 16:26:28 -0400

    feat: add users to Firebase databse

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ShopPage from './pages/shop/shop.component'
 import SignPage from './pages/sign-page/sign-page.component'
 
 import Header from './components/header/header.component'
-import {auth} from './firebase/firebase.utils'
+import {auth, createUserProfileDocument} from './firebase/firebase.utils'
 
 
 class App extends React.Component {
@@ -22,8 +22,9 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user})
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      createUserProfileDocument(user)
+      // this.setState({currentUser: user})
       console.log(user)
     })
   }
