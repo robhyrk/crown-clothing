e6601c970b9e310098fbfd7ada2822ec9a870ef4
Rob (Work) <robhyrk@Robs-MacBook-Pro.local>
2022-04-14 13:58:18 -0400

    add persistor

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,36 @@
 import {compose, createStore, applyMiddleware} from 'redux'
+import {persistStore, persistReducer} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import logger from 'redux-logger'
+// for more customized logging
+// import { loggerMiddleware } from '../middleware/logger'
 
 import {rootReducer} from './root-reducer'
 
-const middleWares = [logger]
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+}
 
-const composedEnhancers = compose(applyMiddleware(...middleWares))
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
+    Boolean
+)
+// for more customized logging
+// const middleWares = [process.env.NODE_ENV === 'development' && loggerMiddleware].filter(
+//     Boolean
+// )
+
+const composedEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ) || compose
+
+const composedEnhancers = composedEnhancer(applyMiddleware(...middleWares))
+
+//for redux persist for saving in local storage
+// export const store = createStore(persistedReducer, undefined, composedEnhancers)
 
 export const store = createStore(rootReducer, undefined, composedEnhancers)
+
+//for redux persist for saving in local storage
+// export const persistor = persistStore(store)
