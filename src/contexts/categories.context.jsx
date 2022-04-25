88228291322383d282941c5fba3182957fc355fd
Rob (Work) <robhyrk@Robs-MacBook-Pro.local>
2022-04-25 18:47:03 -0400

    refactor with graphql

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,24 +1,43 @@
 import { createContext, useState, useEffect } from 'react';
-
-import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+import {gql, useQuery} from '@apollo/client'
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
+const COLLECTIONS = gql`
+  query {
+    collections{
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`
+
 export const CategoriesProvider = ({ children }) => {
+  const {loading, error, data} = useQuery(COLLECTIONS)
   const [categoriesMap, setCategoriesMap] = useState({});
 
-  useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      setCategoriesMap(categoryMap);
-    };
+  console.log(data)
+  useEffect(()=>{
+    if(data){
+      const {collections} = data
+      const collectionsMap = collections.reduce((acc, collection)=>{
+        const {title, items} = collection
+        acc[title.toLowerCase()] = items
+        return acc
+      },{})
+      setCategoriesMap(collectionsMap)
+    }
+  },[data])
 
-    getCategoriesMap();
-  }, []);
-
-  const value = { categoriesMap };
+  const value = { categoriesMap, loading };
   return (
     <CategoriesContext.Provider value={value}>
       {children}
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,30 +1,65 @@
 import { useContext, useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
+import {gql, useQuery} from '@apollo/client'
 
 import ProductCard from '../../components/product-card/product-card.component';
+import Spinner from '../../components/spinner/spinner.component';
 
 import { CategoriesContext } from '../../contexts/categories.context';
 
 import { CategoryContainer, Title } from './category.styles';
 
+const GET_CATEGORY = gql`
+query($title: String!) {
+  getCollectionsByTitle(title: $title){
+    id
+    title
+    items {
+      id
+      name
+      price
+      imageUrl
+    }
+  }
+}
+`
+
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
-  const [products, setProducts] = useState(categoriesMap[category]);
+  // const { categoriesMap, loading } = useContext(CategoriesContext);
 
-  useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [category, categoriesMap]);
+  const {loading, error, data} = useQuery(GET_CATEGORY, {
+    variables: {
+      title: category
+    }
+  })
+
+  useEffect(()=>{
+    if(data) {
+      const {getCollectionsByTitle: {items}} = data
+      setProducts(items)
+    }
+  })
+
+  console.log(data)
+  const [products, setProducts] = useState([]);
+
+  // useEffect(() => {
+  //   setProducts(categoriesMap[category]);
+  // }, [category, categoriesMap]);
 
   return (
     <Fragment>
-      <Title>{category.toUpperCase()}</Title>
-      <CategoryContainer>
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </CategoryContainer>
+      {loading ? <Spinner/> :
+      <Fragment>
+        <Title>{category.toUpperCase()}</Title>
+        <CategoryContainer>
+          {products &&
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </CategoryContainer>
+      </Fragment>}
     </Fragment>
   );
 };
